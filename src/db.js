47d436ab5019d1bf3ccae626bb6869af20dd0e03
Bryan Panchi Zapata <63554719+BryanPanchiZapata@users.noreply.github.com>
2025-05-27 18:23:02 -0500

    variables de entorno

--- src/db.js
+++ src/db.js
@@ -1,10 +1,23 @@
 import pg from "pg";
 
 import { USER, PASSWORD, HOST, DATABASE, DB_PORT } from "./config.js";
-export const pool = new pg.Pool({
+
+const { Pool } = pg;
+let pool;
+if (process.env.NODE_ENV === "production") {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  });
+} else {
+  pool = new Pool({});
+}
+ pool = new pg.Pool({
   user: USER,
   password: PASSWORD,
   host: HOST,
   database: DATABASE,
   port: DB_PORT,
 });
+
+export { pool };

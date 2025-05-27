15fa01ee7a65ee4bcd544cc4e525af79c6ce00ab
Bryan Panchi Zapata <63554719+BryanPanchiZapata@users.noreply.github.com>
2025-05-27 16:38:54 -0500

    desplegar

--- src/db.js
+++ src/db.js
@@ -1,12 +1,10 @@
 import pg from "pg";
-import {USER, PASSWORD, HOST, DATABASE, DB_PORT} from "./config.js";
-
+import { USER, PASSWORD, HOST, DATABASE, DB_PORT } from "./config.js";
 export const pool = new pg.Pool({
   user: USER,
   password: PASSWORD,
   host: HOST,
   database: DATABASE,
-  port:DB_PORT,
-  ssl: process.env.SSL === 'true' ? { rejectUnauthorized: false } : false,
+  port: DB_PORT,
+  ssl: process.env.SSL === "true" ? { rejectUnauthorized: false } : false,
 });
-
